30654eb7b3e6dec0fac73d86428426a5e5cf7fde
itmesaroj <itsmesaroj007@gmail.com>
2024-12-09 20:54:40 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,43 @@
-import './App.css';
-import Home from './component/Home';
-import Navbar from './component/Navbar';
+import './App.css'
+import Home from './component/Home'
+import Navbar from './component/Navbar'
 import { useContext, useEffect } from 'react';
 import ThemeContext from './Context/themeContext';
 import About from './component/About';
-import Aos from 'aos';
-import "aos/dist/aos.css";
+import Aos from 'aos'
+import "aos/dist/aos.css"
 import Skills from './component/Skills';
 import Project from './component/Project';
-import { ContactUs } from './component/Contact';
-import Footer from './component/Footer';
-
+import { ContactUs } from './component/Contect';
+import Fotter from './component/Fotter';
 function App() {
-  useEffect(() => {
-    Aos.init();
-  }, []);
-
-  const { dark } = useContext(ThemeContext);
-  const style = {
+  useEffect(()=>{
+Aos.init()
+  },[])
+  const { dark, toggle } = useContext(ThemeContext);
+  const Style = {
     color: dark ? 'white' : 'RGB(0, 0, 0)',
     backgroundColor: dark ? '#11001F' : 'white',
-  };
-
-  return (
+   
+};
+  return(
     <>
-      <Navbar />
-      <div className="main-container" style={style}>
-        <Home />
-        <br />
-        <About />
-        <br />
-        <Skills />
-        <br />
-        <Project />
-        <br />
-        <ContactUs />
-        <br />
-        <Footer />
-      </div>
+    <Navbar/>
+    <div className="main-conatiner" style={Style}>
+      <Home/>
+      <br />
+      <About/>
+      <br />
+    <Skills/>
+    <br />
+    <Project/>
+    <br />
+    <ContactUs/>
+    <br />
+    <Fotter/>
+    </div>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
